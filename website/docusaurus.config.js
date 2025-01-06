8bb14f109c481ddc049b289b5eedd71abb447336
Spencer Y <love4yzp@gmail.com>
2025-01-06 11:05:50 +0800

    add deploy

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = { // Front info
   favicon: 'img/seeedstudio.webp', // TODO: change to seeedstudio logo .ico
 
   // Set the production url of your site here
-  url: 'https://seeed-studio.github.io',
+  url: 'https://love4yzp.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Hazard-Response-Mission-Pack',
@@ -24,6 +24,7 @@ const config = { // Front info
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Seeed-Studio', // Usually your GitHub org/user name.
   projectName: 'Hazard-Response-Mission-Pack', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
